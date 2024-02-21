f10b7cb45783ecfb31a844224d9730038b0255a4
Jan Neduchal <duchy@honeypot.lol>
2024-02-21 09:54:58 +0000

    Update kvm-install.sh

--- kvm-install.sh
+++ kvm-install.sh
@@ -1,4 +1,4 @@
-sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
 sudo adduser $USER libvirt
 sudo adduser $USER kvm
 
